383d48c0ea1baab3db65a778d1e15dc895acd880
Juan Manuel Perez <jmperez@dc.uba.ar>
2018-05-09 17:47:25 -0300

    Regions for words.

--- bin/02_generate_lists.py
+++ bin/02_generate_lists.py
@@ -4,10 +4,9 @@ Note: see https://github.com/daleman/tesis/blob/master/notebooks/info.ipynb
 """
 
 import fire
-import numpy as np
-from scipy.stats import entropy
 from contrastes import read_occurrence_dataframe
 from contrastes.information_value import information_value
+
 
 def generate_lists(
     input_path="output/provinces_words.csv",
@@ -22,10 +21,11 @@ def generate_lists(
     """
     df = read_occurrence_dataframe(input_path, filter_words=True)
 
-    df["ival_palabras"] = information_value(df, "cant_palabra", df.cant_palabras)
-    df["ival_personas"] = information_value(df, "cant_usuarios", df.cant_personas)
+    df["ival_palabras"] = information_value(df, "cant_palabra",
+                                            df.cant_palabras)
+    df["ival_personas"] = information_value(df, "cant_usuarios",
+                                            df.cant_personas)
     df["ival"] = df["ival_palabras"] * df["ival_personas"]
-
 
     columns = [
         "cant_palabra",
@@ -36,19 +36,20 @@ def generate_lists(
     lists = {
         "personas": df.sort_values("ival_personas", ascending=False),
         "palabras": df.sort_values("ival_palabras", ascending=False),
-        "palabras_personas" : df.sort_values("ival", ascending=False)
+        "palabras_personas": df.sort_values("ival", ascending=False)
 
     }
 
     for k, df_sorted in lists.items():
         output_path = "output/listado_{}.csv".format(k)
         df_sorted["posicion"] = range(1, len(df)+1)
+        print("Generating {}".format(output_path))
 
         df_sorted.to_csv(
             output_path,
-            columns=columns,
             index_label="palabra",
         )
+
 
 if __name__ == '__main__':
     fire.Fire(generate_lists)
