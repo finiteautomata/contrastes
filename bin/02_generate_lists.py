a9a52da2ab41fe8124d79da0e4c9a8c39ead906d
Juan Manuel Pérez <jmperez.85@gmail.com>
2018-05-29 13:40:01 -0300

    Send function to module

--- bin/02_generate_lists.py
+++ bin/02_generate_lists.py
@@ -6,56 +6,16 @@ Note: see https://github.com/daleman/tesis/blob/master/notebooks/info.ipynb
 import fire
 import os
 from contrastes import read_occurrence_dataframe
-from contrastes.lists import add_info
+from contrastes.lists import add_info, save_unlabeled_list
+
+
+def save_word_fnorm(df, output_path):
+    pass
 
 
 def save_lists(df, output_path):
-    """
-    Save lists.
-
-    Arguments:
-    ---------
-
-    df: pandas.DataFrame
-        Occurrence dataframe, with added info
-    """
-
-    word_list = df[(df.rank_palabras <= 1000) |\
-                   (df.rank_personas <= 1000) |\
-                   (df.rank_palper <= 1000)]
-
-    not_labeled = word_list[word_list.etiqueta.isna()].copy()
-
-    # Add extra info
-    not_labeled["lugar"] = not_labeled.es_lugar.apply(
-        lambda w: "lugar" if w else "ok"
-    )
-    not_labeled["provincias_sin_esa_palabra"] = 23 - not_labeled.cant_provincias
-
-    columns=["region", "lugar", "provincias_sin_esa_palabra"]
-
-    print("Lists of first 1000 words")
-    print("Number of total words (without repetition): {}".format(len(word_list)))
-    print("Not labeled:{} palabras".format(not_labeled.shape[0]))
-    print("Those of which {} are places".format(sum(not_labeled.es_lugar)))
-
-    complete_path = os.path.join(output_path, "1000_no_etiquetadas_completo.csv")
-    reduced_path = os.path.join(output_path, "1000_no_etiquetadas_random_reducido.csv")
-
-    reordered_columns = columns + list(not_labeled.columns.difference(columns))
-
-    not_labeled.to_csv(complete_path,
-                       columns=reordered_columns)
-    print("Not labeled full list saved to {}".format(complete_path))
-    # This shuffles the dataframe
-    shuffled = not_labeled.sample(frac=1).copy()
-
-
-    shuffled.to_csv(
-        reduced_path,
-        columns=columns
-    )
-    print("Not labeled shuffled reduced list saved to {}".format(reduced_path))
+    save_unlabeled_list(df, output_path, threshold=1000)
+    save_word_fnorm(df, output_path)
 
 
 def generate_lists(
--- contrastes/lists.py
+++ contrastes/lists.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from .information_value import information_value
 from .geo import places, region
@@ -11,13 +12,17 @@ def get_label():
     label[label == "0,5"] = "0.5"
     return label.astype("float")
 
-def add_info(df):
-    """
-    Add information value and other stuff
-    """
-    print("Calculating entropy, regions, and stuff...")
-    lugares = places()
 
+def _add_fnorm(df):
+    for col in df.cant_palabras:
+        total_sum = df[col].sum()
+        prov = col.split("_")[0]
+        fnorm_col = "fnorm_{}".format(prov)
+
+        df[fnorm_col] = df[col] * (1000000.0 / total_sum)
+
+
+def _add_ival(df):
     df["ival_palabras"] = information_value(df, "cant_palabra",
                                             df.cant_palabras)
     df["ival_personas"] = information_value(df, "cant_usuarios",
@@ -28,6 +33,71 @@ def add_info(df):
     df["rank_personas"] = df["ival_personas"].rank(ascending=False)
     df["rank_palper"] = df["ival_palper"].rank(ascending=False)
 
+
+def add_info(df):
+    """
+    Add information value and other stuff
+    """
+    print("Calculating entropy, regions, and stuff...")
+    _add_ival(df)
+
     df["region"] = df[df.cant_personas].apply(region, axis=1)
+
+    lugares = places()
     df["es_lugar"] = df.index.map(lambda w: w in lugares)
     df["etiqueta"] = get_label()
+    _add_fnorm(df)
+
+
+def save_unlabeled_list(df, output_path, threshold=1000):
+    """
+    Save list of unlabeled words.
+
+    Arguments:
+    ---------
+
+    df: pandas.DataFrame
+        Occurrence dataframe, with added info
+    """
+
+    word_list = df[(df.rank_palabras <= threshold) |
+                   (df.rank_personas <= threshold) |
+                   (df.rank_palper <= threshold)]
+
+    not_labeled = word_list[word_list.etiqueta.isna()].copy()
+
+    # Add extra info
+    not_labeled["lugar"] = not_labeled.es_lugar.apply(
+        lambda w: "lugar" if w else "ok"
+    )
+
+    not_labeled["provincias_sin_esa_palabra"] = 23 -\
+        not_labeled.cant_provincias
+
+    columns = ["region", "lugar", "provincias_sin_esa_palabra"]
+
+    print("Lists of first {} words".format(threshold))
+    print(
+        "Number of total words (without repetition): {}".format(len(word_list))
+    )
+    print("Not labeled:{} palabras".format(not_labeled.shape[0]))
+    print("Those of which {} are places".format(sum(not_labeled.es_lugar)))
+
+    complete_path = os.path.join(output_path,
+                                 "1000_no_etiquetadas_completo.csv")
+    reduced_path = os.path.join(output_path,
+                                "1000_no_etiquetadas_random_reducido.csv")
+
+    reordered_columns = columns + list(not_labeled.columns.difference(columns))
+
+    not_labeled.to_csv(complete_path,
+                       columns=reordered_columns)
+    print("Not labeled full list saved to {}".format(complete_path))
+    # This shuffles the dataframe
+    shuffled = not_labeled.sample(frac=1).copy()
+
+    shuffled.to_csv(
+        reduced_path,
+        columns=columns
+    )
+    print("Not labeled shuffled reduced list saved to {}".format(reduced_path))
